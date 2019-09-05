3a9d2a86c7d41c75b0c9bbd8512efbceb33079b1
Aman Mittal <amandeepmittal@live.com>
2019-09-05 15:30:59 +0530

    add error message and disable butto func

---
+++ components/ErrorMessage.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import { View, Text, StyleSheet } from 'react-native'
+
+const ErrorMessage = ({ errorValue }) => (
+  <View style={styles.container}>
+    <Text style={styles.errorText}>{errorValue}</Text>
+  </View>
+)
+
+const styles = StyleSheet.create({
+  container: {
+    marginLeft: 25
+  },
+  errorText: {
+    color: 'red'
+  }
+})
+
+export default ErrorMessage
--- screens/Login.js
+++ screens/Login.js
@@ -2,8 +2,21 @@ import React, { Fragment } from 'react'
 import { StyleSheet, SafeAreaView, View } from 'react-native'
 import { Button } from 'react-native-elements'
 import { Formik } from 'formik'
+import * as Yup from 'yup'
 import FormInput from '../components/FormInput'
 import FormButton from '../components/FormButton'
+import ErrorMessage from '../components/ErrorMessage'
+
+const validationSchema = Yup.object().shape({
+  email: Yup.string()
+    .label('Email')
+    .email('Enter a valid email')
+    .required('Please enter a registered email'),
+  password: Yup.string()
+    .label('Password')
+    .required()
+    .min(4, 'Password must have more than 4 characters ')
+})
 
 export default class Login extends React.Component {
   goToSignup = () => this.props.navigation.navigate('Signup')
@@ -21,8 +34,9 @@ export default class Login extends React.Component {
           initialValues={{ email: '', password: '' }}
           onSubmit={values => {
             this.handleSubmit(values)
-          }}>
-          {({ handleChange, values, handleSubmit }) => (
+          }}
+          validationSchema={validationSchema}>
+          {({ handleChange, values, handleSubmit, errors, isValid }) => (
             <Fragment>
               <FormInput
                 name='email'
@@ -33,6 +47,7 @@ export default class Login extends React.Component {
                 iconName='ios-mail'
                 iconColor='#2C384A'
               />
+              <ErrorMessage errorValue={errors.email} />
               <FormInput
                 name='password'
                 value={values.password}
@@ -42,12 +57,14 @@ export default class Login extends React.Component {
                 iconName='ios-lock'
                 iconColor='#2C384A'
               />
+              <ErrorMessage errorValue={errors.password} />
               <View style={styles.buttonContainer}>
                 <FormButton
                   buttonType='outline'
                   onPress={handleSubmit}
                   title='LOGIN'
                   buttonColor='#039BE5'
+                  disabled={!isValid}
                 />
               </View>
             </Fragment>
