25a3a2c0a9ec273054f8d5e314df6f04661da9b5
Daniel Cunha <danielbrandaodacunha@gmail.com>
2019-10-02 09:09:41 -0300

    Signup UX improvement

--- screens/Signup.js
+++ screens/Signup.js
@@ -1,5 +1,5 @@
 import React, { Component, Fragment } from 'react'
-import { StyleSheet, SafeAreaView, View, TouchableOpacity } from 'react-native'
+import { StyleSheet, SafeAreaView, View, TouchableOpacity, KeyboardAvoidingView } from 'react-native'
 import { Button, CheckBox } from 'react-native-elements'
 import { Ionicons } from '@expo/vector-icons'
 import { Formik } from 'formik'
@@ -85,124 +85,136 @@ class Signup extends Component {
       confirmPasswordIcon
     } = this.state
     return (
-      <SafeAreaView style={styles.container}>
-        <Formik
-          initialValues={{
-            name: '',
-            email: '',
-            password: '',
-            confirmPassword: '',
-            check: false
-          }}
-          onSubmit={(values, actions) => {
-            this.handleOnSignup(values, actions)
-          }}
-          validationSchema={validationSchema}>
-          {({
-            handleChange,
-            values,
-            handleSubmit,
-            errors,
-            isValid,
-            touched,
-            handleBlur,
-            isSubmitting,
-            setFieldValue
-          }) => (
-            <Fragment>
-              <FormInput
-                name='name'
-                value={values.name}
-                onChangeText={handleChange('name')}
-                placeholder='Enter your full name'
-                iconName='md-person'
-                iconColor='#2C384A'
-                onBlur={handleBlur('name')}
-              />
-              <ErrorMessage errorValue={touched.name && errors.name} />
-              <FormInput
-                name='email'
-                value={values.email}
-                onChangeText={handleChange('email')}
-                placeholder='Enter email'
-                autoCapitalize='none'
-                iconName='ios-mail'
-                iconColor='#2C384A'
-                onBlur={handleBlur('email')}
-              />
-              <ErrorMessage errorValue={touched.email && errors.email} />
-              <FormInput
-                name='password'
-                value={values.password}
-                onChangeText={handleChange('password')}
-                placeholder='Enter password'
-                iconName='ios-lock'
-                iconColor='#2C384A'
-                onBlur={handleBlur('password')}
-                secureTextEntry={passwordVisibility}
-                rightIcon={
-                  <TouchableOpacity onPress={this.handlePasswordVisibility}>
-                    <Ionicons name={passwordIcon} size={28} color='grey' />
-                  </TouchableOpacity>
-                }
-              />
-              <ErrorMessage errorValue={touched.password && errors.password} />
-              <FormInput
-                name='password'
-                value={values.confirmPassword}
-                onChangeText={handleChange('confirmPassword')}
-                placeholder='Confirm password'
-                iconName='ios-lock'
-                iconColor='#2C384A'
-                onBlur={handleBlur('confirmPassword')}
-                secureTextEntry={confirmPasswordVisibility}
-                rightIcon={
-                  <TouchableOpacity
-                    onPress={this.handleConfirmPasswordVisibility}>
-                    <Ionicons
-                      name={confirmPasswordIcon}
-                      size={28}
-                      color='grey'
+
+
+      <KeyboardAvoidingView
+        style={styles.container}
+        enabled
+        behavior='position'
+      >
+        <SafeAreaView>
+          <Formik
+            initialValues={{
+              name: '',
+              email: '',
+              password: '',
+              confirmPassword: '',
+              check: false
+            }}
+            onSubmit={(values, actions) => {
+              this.handleOnSignup(values, actions)
+            }}
+            validationSchema={validationSchema}>
+            {({
+              handleChange,
+              values,
+              handleSubmit,
+              errors,
+              isValid,
+              touched,
+              handleBlur,
+              isSubmitting,
+              setFieldValue
+            }) => (
+                <Fragment>
+
+                  <FormInput
+                    name='name'
+                    value={values.name}
+                    onChangeText={handleChange('name')}
+                    placeholder='Enter your full name'
+                    iconName='md-person'
+                    iconColor='#2C384A'
+                    onBlur={handleBlur('name')}
+                  />
+                  <ErrorMessage errorValue={touched.name && errors.name} />
+                  <FormInput
+                    name='email'
+                    value={values.email}
+                    onChangeText={handleChange('email')}
+                    placeholder='Enter email'
+                    autoCapitalize='none'
+                    iconName='ios-mail'
+                    iconColor='#2C384A'
+                    onBlur={handleBlur('email')}
+                  />
+                  <ErrorMessage errorValue={touched.email && errors.email} />
+                  <FormInput
+                    name='password'
+                    value={values.password}
+                    onChangeText={handleChange('password')}
+                    placeholder='Enter password'
+                    iconName='ios-lock'
+                    iconColor='#2C384A'
+                    onBlur={handleBlur('password')}
+                    secureTextEntry={passwordVisibility}
+                    rightIcon={
+                      <TouchableOpacity onPress={this.handlePasswordVisibility}>
+                        <Ionicons name={passwordIcon} size={28} color='grey' />
+                      </TouchableOpacity>
+                    }
+                  />
+                  <ErrorMessage errorValue={touched.password && errors.password} />
+                  <FormInput
+                    name='password'
+                    value={values.confirmPassword}
+                    onChangeText={handleChange('confirmPassword')}
+                    placeholder='Confirm password'
+                    iconName='ios-lock'
+                    iconColor='#2C384A'
+                    onBlur={handleBlur('confirmPassword')}
+                    secureTextEntry={confirmPasswordVisibility}
+                    rightIcon={
+                      <TouchableOpacity
+                        onPress={this.handleConfirmPasswordVisibility}>
+                        <Ionicons
+                          name={confirmPasswordIcon}
+                          size={28}
+                          color='grey'
+                        />
+                      </TouchableOpacity>
+                    }
+                  />
+                  <ErrorMessage
+                    errorValue={touched.confirmPassword && errors.confirmPassword}
+                  />
+                  <CheckBox
+                    containerStyle={styles.checkBoxContainer}
+                    checkedIcon='check-box'
+                    iconType='material'
+                    uncheckedIcon='check-box-outline-blank'
+                    title='Agree to terms and conditions'
+                    checkedTitle='You agreed to our terms and conditions'
+                    checked={values.check}
+                    onPress={() => setFieldValue('check', !values.check)}
+                  />
+                  <View style={styles.buttonContainer}>
+                    <FormButton
+                      buttonType='outline'
+                      onPress={handleSubmit}
+                      title='SIGNUP'
+                      buttonColor='#F57C00'
+                      disabled={!isValid || isSubmitting}
+                      loading={isSubmitting}
                     />
-                  </TouchableOpacity>
-                }
-              />
-              <ErrorMessage
-                errorValue={touched.confirmPassword && errors.confirmPassword}
-              />
-              <CheckBox
-                containerStyle={styles.checkBoxContainer}
-                checkedIcon='check-box'
-                iconType='material'
-                uncheckedIcon='check-box-outline-blank'
-                title='Agree to terms and conditions'
-                checkedTitle='You agreed to our terms and conditions'
-                checked={values.check}
-                onPress={() => setFieldValue('check', !values.check)}
-              />
-              <View style={styles.buttonContainer}>
-                <FormButton
-                  buttonType='outline'
-                  onPress={handleSubmit}
-                  title='SIGNUP'
-                  buttonColor='#F57C00'
-                  disabled={!isValid || isSubmitting}
-                  loading={isSubmitting}
-                />
-              </View>
-              <ErrorMessage errorValue={errors.general} />
-            </Fragment>
-          )}
-        </Formik>
-        <Button
-          title='Have an account? Login'
-          onPress={this.goToLogin}
-          titleStyle={{
-            color: '#039BE5'
-          }}
-          type='clear'
-        />
-      </SafeAreaView>
+                  </View>
+                  <ErrorMessage errorValue={errors.general} />
+                </Fragment>
+              )}
+          </Formik>
+          <Button
+            title='Have an account? Login'
+            onPress={this.goToLogin}
+            titleStyle={{
+              color: '#039BE5'
+            }}
+            type='clear'
+          />
+        </SafeAreaView>
+
+      </KeyboardAvoidingView>
+
+
     )
   }
 }
@@ -211,7 +223,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    marginTop: 50
+    marginTop: 50,
   },
   logoContainer: {
     marginBottom: 15,
