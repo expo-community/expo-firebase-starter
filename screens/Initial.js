a7b96297faf04f5e03f95101c79acdda3745b3c4
Aman Mittal <amandeepmittal@live.com>
2020-03-22 12:15:08 +0530

    refactor: convert class to functional component

--- screens/Initial.js
+++ screens/Initial.js
@@ -1,65 +1,117 @@
-import React, { Component } from 'react'
-import { AppLoading } from 'expo'
-import { Asset } from 'expo-asset'
-import * as Font from 'expo-font'
-import * as Icon from '@expo/vector-icons'
-import { withFirebaseHOC } from '../config/Firebase'
+import React, { useState, useEffect } from "react";
+import { AppLoading } from "expo";
+import { Asset } from "expo-asset";
+import * as Font from "expo-font";
+import * as Icon from "@expo/vector-icons";
+import { withFirebaseHOC } from "../config/Firebase";
 
-class Initial extends Component {
-  state = {
-    isAssetsLoadingComplete: false
-  }
+function Initial({ navigation, firebase }) {
+  const [isAssetsLoadingComplete, setIsAssetsLoadingComplete] = useState(false);
 
-  componentDidMount = async () => {
+  useEffect(() => {
     try {
-      // previously
-      this.loadLocalAsync()
+      loadLocalAsync();
 
-      await this.props.firebase.checkUserAuth(user => {
+      firebase.checkUserAuth(user => {
         if (user) {
           // if the user has previously logged in
-          this.props.navigation.navigate('App')
+          navigation.navigate("App");
         } else {
-          // if the user has previously signed out from the app
-          this.props.navigation.navigate('Auth')
+          // if the user has previously logged out from the app
+          navigation.navigate("Auth");
         }
-      })
+      });
     } catch (error) {
-      console.log(error)
+      console.log(error);
     }
-  }
+  }, []);
 
-  loadLocalAsync = async () => {
+  async function loadLocalAsync() {
     return await Promise.all([
       Asset.loadAsync([
-        require('../assets/flame.png'),
-        require('../assets/icon.png')
+        require("../assets/flame.png"),
+        require("../assets/icon.png")
       ]),
       Font.loadAsync({
         ...Icon.Ionicons.font
       })
-    ])
+    ]);
   }
 
-  handleLoadingError = error => {
+  function handleLoadingError(error) {
     // In this case, you might want to report the error to your error
     // reporting service, for example Sentry
-    console.warn(error)
+    console.warn(error);
   }
 
-  handleFinishLoading = () => {
-    this.setState({ isAssetsLoadingComplete: true })
+  function handleFinishLoading() {
+    setIsAssetsLoadingComplete(true);
   }
 
-  render() {
-    return (
-      <AppLoading
-        startAsync={this.loadLocalAsync}
-        onFinish={this.handleFinishLoading}
-        onError={this.handleLoadingError}
-      />
-    )
-  }
+  return (
+    <AppLoading
+      startAsync={loadLocalAsync}
+      onFinish={handleFinishLoading}
+      onError={handleLoadingError}
+    />
+  );
 }
 
-export default withFirebaseHOC(Initial)
+// class Initial extends Component {
+//   state = {
+//     isAssetsLoadingComplete: false
+//   }
+
+//   componentDidMount = async () => {
+//     try {
+//       // previously
+//       this.loadLocalAsync()
+
+//       await this.props.firebase.checkUserAuth(user => {
+//         if (user) {
+//           // if the user has previously logged in
+//           this.props.navigation.navigate('App')
+//         } else {
+//           // if the user has previously signed out from the app
+//           this.props.navigation.navigate('Auth')
+//         }
+//       })
+//     } catch (error) {
+//       console.log(error)
+//     }
+//   }
+
+//   loadLocalAsync = async () => {
+//     return await Promise.all([
+//       Asset.loadAsync([
+//         require('../assets/flame.png'),
+//         require('../assets/icon.png')
+//       ]),
+//       Font.loadAsync({
+//         ...Icon.Ionicons.font
+//       })
+//     ])
+//   }
+
+//   handleLoadingError = error => {
+//     // In this case, you might want to report the error to your error
+//     // reporting service, for example Sentry
+//     console.warn(error)
+//   }
+
+//   handleFinishLoading = () => {
+//     this.setState({ isAssetsLoadingComplete: true })
+//   }
+
+//   render() {
+//     return (
+//       <AppLoading
+//         startAsync={this.loadLocalAsync}
+//         onFinish={this.handleFinishLoading}
+//         onError={this.handleLoadingError}
+//       />
+//     )
+//   }
+// }
+
+export default withFirebaseHOC(Initial);
