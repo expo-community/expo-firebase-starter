9c87f0ada16ed834847c767bc3909870ca2d77db
Aman Mittal <amandeepmittal@live.com>
2020-03-22 12:17:54 +0530

    refactor: convert class to functional component

--- screens/Home.js
+++ screens/Home.js
@@ -1,41 +1,40 @@
-import React, { Component } from 'react'
-import { StyleSheet, Text, View } from 'react-native'
-import { Button } from 'react-native-elements'
-import { withFirebaseHOC } from '../config/Firebase'
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import { Button } from "react-native-elements";
+import { withFirebaseHOC } from "../config/Firebase";
 
-class Home extends Component {
-  handleSignout = async () => {
+function Home({ navigation, firebase }) {
+  async function handleSignout() {
     try {
-      await this.props.firebase.signOut()
-      this.props.navigation.navigate('Auth')
+      await firebase.signOut();
+      navigation.navigate("Auth");
     } catch (error) {
-      console.log(error)
+      console.log(error);
     }
   }
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text>Home</Text>
-        <Button
-          title='Signout'
-          onPress={this.handleSignout}
-          titleStyle={{
-            color: '#F57C00'
-          }}
-          type='clear'
-        />
-      </View>
-    )
-  }
+
+  return (
+    <View style={styles.container}>
+      <Text>Home</Text>
+      <Button
+        title="Signout"
+        onPress={handleSignout}
+        titleStyle={{
+          color: "#F57C00"
+        }}
+        type="clear"
+      />
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center'
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center"
   }
-})
+});
 
-export default withFirebaseHOC(Home)
+export default withFirebaseHOC(Home);
