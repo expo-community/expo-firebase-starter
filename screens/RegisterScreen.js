4c19cca2c069ac3080dc6e1730339277382393e1
Aman Mittal <amandeepmittal@live.com>
2020-06-14 18:17:47 +0530

    chore: refactor register screen

---
+++ screens/RegisterScreen.js
@@ -0,0 +1,116 @@
+import React, { useState } from 'react';
+import { StyleSheet } from 'react-native';
+import * as Yup from 'yup';
+
+import Colors from '../utils/colors';
+import SafeView from '../components/SafeView';
+import Form from '../components/Forms/Form';
+import FormField from '../components/Forms/FormField';
+import FormButton from '../components/Forms/FormButton';
+
+const validationSchema = Yup.object().shape({
+  name: Yup.string()
+    .required()
+    .label('Name'),
+  email: Yup.string()
+    .required('Please enter a registered email')
+    .email()
+    .label('Email'),
+  password: Yup.string()
+    .required()
+    .min(6, 'Password must have at least 6 characters')
+    .label('Password'),
+  confirmPassword: Yup.string()
+    .oneOf([Yup.ref('password')], 'Confirm Password must matched Password')
+    .required('Confirm Password is required'),
+  check: Yup.boolean().oneOf([true], 'Please check the agreement')
+});
+
+export default function RegisterScreen() {
+  const [passwordVisibility, setPasswordVisibility] = useState(true);
+  const [rightIcon, setRightIcon] = useState('eye');
+  const [confirmPasswordIcon, setConfirmPasswordIcon] = useState('eye');
+  const [confirmPasswordVisibility, setConfirmPasswordVisibility] = useState(
+    true
+  );
+
+  function handlePasswordVisibility() {
+    if (rightIcon === 'eye') {
+      setRightIcon('eye-off');
+      setPasswordVisibility(!passwordVisibility);
+    } else if (rightIcon === 'eye-off') {
+      setRightIcon('eye');
+      setPasswordVisibility(!passwordVisibility);
+    }
+  }
+
+  function handleConfirmPasswordVisibility() {
+    if (confirmPasswordIcon === 'eye') {
+      setConfirmPasswordIcon('eye-off');
+      setConfirmPasswordVisibility(!confirmPasswordVisibility);
+    } else if (confirmPasswordIcon === 'eye-off') {
+      setConfirmPasswordIcon('eye');
+      setConfirmPasswordVisibility(!confirmPasswordVisibility);
+    }
+  }
+
+  return (
+    <SafeView style={styles.container}>
+      <Form
+        initialValues={{
+          name: '',
+          email: '',
+          password: '',
+          confirmPassword: ''
+        }}
+        validationSchema={validationSchema}
+        onSubmit={values => console.log(values)}
+      >
+        <FormField
+          name="name"
+          leftIcon="account"
+          placeholder="Enter name"
+          autoFocus={true}
+        />
+        <FormField
+          name="email"
+          leftIcon="email"
+          placeholder="Enter email"
+          autoCapitalize="none"
+          keyboardType="email-address"
+          textContentType="emailAddress"
+        />
+        <FormField
+          name="password"
+          leftIcon="lock"
+          placeholder="Enter password"
+          autoCapitalize="none"
+          autoCorrect={false}
+          secureTextEntry={passwordVisibility}
+          textContentType="password"
+          rightIcon={rightIcon}
+          handlePasswordVisibility={handlePasswordVisibility}
+        />
+        <FormField
+          name="confirmPassword"
+          leftIcon="lock"
+          placeholder="Confirm password"
+          autoCapitalize="none"
+          autoCorrect={false}
+          secureTextEntry={confirmPasswordVisibility}
+          textContentType="password"
+          rightIcon={confirmPasswordIcon}
+          handlePasswordVisibility={handleConfirmPasswordVisibility}
+        />
+        <FormButton title={'Register'} />
+      </Form>
+    </SafeView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    padding: 15,
+    backgroundColor: Colors.mediumGrey
+  }
+});
