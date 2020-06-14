21ee7eaf992ca1557475f30300a4688857c8c142
Aman Mittal <amandeepmittal@live.com>
2020-06-14 18:17:26 +0530

    chore: refactor login screen

---
+++ screens/LoginScreen.js
@@ -0,0 +1,74 @@
+import React, { useState } from 'react';
+import { StyleSheet } from 'react-native';
+import * as Yup from 'yup';
+
+import Colors from '../utils/colors';
+import SafeView from '../components/SafeView';
+import Form from '../components/Forms/Form';
+import FormField from '../components/Forms/FormField';
+import FormButton from '../components/Forms/FormButton';
+
+const validationSchema = Yup.object().shape({
+  email: Yup.string()
+    .required('Please enter a registered email')
+    .email()
+    .label('Email'),
+  password: Yup.string()
+    .required()
+    .min(6, 'Password must have at least 6 characters')
+    .label('Password')
+});
+
+export default function LoginScreen() {
+  const [passwordVisibility, setPasswordVisibility] = useState(true);
+  const [rightIcon, setRightIcon] = useState('eye');
+
+  function handlePasswordVisibility() {
+    if (rightIcon === 'eye') {
+      setRightIcon('eye-off');
+      setPasswordVisibility(!passwordVisibility);
+    } else if (rightIcon === 'eye-off') {
+      setRightIcon('eye');
+      setPasswordVisibility(!passwordVisibility);
+    }
+  }
+
+  return (
+    <SafeView style={styles.container}>
+      <Form
+        initialValues={{ email: '', password: '' }}
+        validationSchema={validationSchema}
+        onSubmit={values => console.log(values)}
+      >
+        <FormField
+          name="email"
+          leftIcon="email"
+          placeholder="Enter email"
+          autoCapitalize="none"
+          keyboardType="email-address"
+          textContentType="emailAddress"
+          autoFocus={true}
+        />
+        <FormField
+          name="password"
+          leftIcon="lock"
+          placeholder="Enter password"
+          autoCapitalize="none"
+          autoCorrect={false}
+          secureTextEntry={passwordVisibility}
+          textContentType="password"
+          rightIcon={rightIcon}
+          handlePasswordVisibility={handlePasswordVisibility}
+        />
+        <FormButton title={'Login'} />
+      </Form>
+    </SafeView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    padding: 15,
+    backgroundColor: Colors.mediumGrey
+  }
+});
