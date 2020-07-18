7c29ca76592ad35a96be7c63917d24786d17b208
Aman Mittal <amandeepmittal@live.com>
2020-07-18 13:32:56 +0530

    🚀 upgrade to Expo SDK 38

--- App.js
+++ App.js
@@ -1,16 +1,5 @@
 import React from 'react';
 
-// export default function App() {
-//   return (
-//     <FirebaseProvider value={Firebase}>
-//       <AppContainer />
-//     </FirebaseProvider>
-//   );
-// }
-
-// TODO: REMOVE THIS and uncomment above
-// WARNING: ONLY FOR MANUAL TESTING
-
 import Providers from './navigation';
 
 export default function App() {
