44288e42176589cabf393f8c60cc37cffa711022
Aman Mittal <amandeepmittal@live.com>
2021-10-24 22:29:02 +0530

    initialize firebase and firebase auth

---
+++ app.config.js
@@ -0,0 +1,33 @@
+import 'dotenv/config';
+
+export default {
+  expo: {
+    name: 'Expo Firebase Starter',
+    slug: 'expo-firebase',
+    privacy: 'public',
+    platforms: ['ios', 'android'],
+    version: '0.15.0',
+    orientation: 'portrait',
+    icon: './assets/flame.png',
+    splash: {
+      image: './assets/splash.png',
+      resizeMode: 'cover',
+      backgroundColor: '#F57C00'
+    },
+    updates: {
+      fallbackToCacheTimeout: 0
+    },
+    assetBundlePatterns: ['**/*'],
+    ios: {
+      supportsTablet: true
+    },
+    extra: {
+      apiKey: process.env.API_KEY,
+      authDomain: process.env.AUTH_DOMAIN,
+      projectId: process.env.PROJECT_ID,
+      storageBucket: process.env.STORAGE_BUCKET,
+      messagingSenderId: process.env.MESSAGING_SENDER_ID,
+      appId: process.env.APP_ID
+    }
+  }
+};
